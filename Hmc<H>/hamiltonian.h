c843dee90d8799afec2b418828473a70cda68923
Noor E Mustafa Ferdous <nooremf@gmail.com>
2022-09-23 08:58:19 +0600

    <H>

---
+++ Hmc<H>/hamiltonian.h
@@ -0,0 +1,49 @@
+#ifndef HAM
+#define HAM
+
+/* 
+    Calculating Hamiltonian
+*/
+double hamiltonian(complex<double> A[n][n], complex<double> B[n][n])
+{
+    complex <double> H[n][n] = {0}, N1[n][n] = {0}, N2[n][n] = {0};
+    double sum=0;
+
+    for(int i=0; i<n; i+=1)
+    {
+        for(int j=0; j<n; j+=1)
+        {
+            for(int k=0; k<n; k+=1)
+            {
+                N1[i][j] += A[i][k]*B[k][j];
+            }
+        }
+    }
+
+    for(int i=0; i<n; i+=1)
+    {
+        for(int j=0; j<n; j+=1)
+        {
+            for(int k=0; k<n; k+=1)
+            {
+                N2[i][j] += B[i][k]*A[k][j];
+            }
+        }
+    }
+
+    for(int i=0; i<n; i+=1)
+    {
+        for(int j=0; j<n; j+=1)
+        {
+            H[i][j] = N1[i][j] + N2[i][j];
+        }
+    }
+
+    for(int i=0; i<n; i+=1)
+    {
+        sum += H[i][i].real();
+    }
+
+    return sum;
+}
+#endif
---
+++ Hmc<H>/plot.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.loadtxt("main.dat")
+
+f = plt.figure()
+f.set_figwidth(10)
+f.set_figheight(8)
+plt.plot(a[:,0], a[:,1])
+plt.xlim([0,100])
+plt.xlabel("iterations", fontsize=20)
+plt.ylabel("<H>/N^2", fontsize=20)
+plt.title("Expectation value of H=(1/2)(xp+px)", fontsize=20)
+plt.show()
+
+
+f = plt.figure()
+f.set_figwidth(10)
+f.set_figheight(8)
+plt.plot(a[:,0], a[:,1])
+plt.xlim([0,100])
+plt.xlabel("iterations", fontsize=20)
+plt.ylabel("<H>/N^2", fontsize=20)
+plt.title("Expectation value of H=(1/2)(xp+px)", fontsize=20)
+plt.savefig("plot.pdf")
