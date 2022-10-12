1df81b3bc9107c14e2052f3ecd63d29fda9411ba
Noor E Mustafa Ferdous <nooremf@gmail.com>
2022-10-12 23:29:38 +0600

    confused

--- Berry-Keating/main.cpp
+++ Berry-Keating/main.cpp
@@ -10,82 +10,99 @@
 #include<ctime>
 using namespace std;
 
+double fact(double x)
+{
+    double s=1;
+    for(int i=1; i<=x; i+=1)
+    {
+        s *= i*(i+1);
+    }
+
+    return s;
+}
+
 int main()
 {
     srand(time(NULL));
     ofstream fout("main.dat");
     ofstream file("part.dat");
-    double hi, hf, s=0, s2=0, r, c=0,p,q,C[10000]={};
+    double hi, hf, s=0, s2=0, r, c=0,p,q,C[10000]={},x=0,a=0;
     long double x1=0, x2, y1=0, y2=0;
     const int n=10;
     //x=0;
     complex<double> A[n][n] = {0}, A0[n][n] = {0},B[n][n] = {0},B0[n][n] = {0};
 
-
-    //generating random value for x
-    for(int i=0; i<n; i+=1)
-    {
-        for(int j=0; j<n; j+=1)
-        {   
-            Box(p,q);
-            A[i][j] = complex(p/sqrt(2),q/sqrt(2));
-        }
-    }
-
-
-    //metropolis test
-    for(int i=1; i<10000; i+=1)
-    {
-        for(int j=0; j<n; j+=1)
+    //#pragma omp parallel for
+    //for(int t=1; t<=10000; t+=100)
+    //{   
+        //generating random value for x
+        for(int i=0; i<n; i+=1)
         {
-            for(int k=0; k<n; k+=1)
+            for(int j=0; j<n; j+=1)
             {   
-                //x0=x
-                A0[j][k] = A[j][k];
-
+                p=(double)rand()/(double)RAND_MAX;
+                q=(double)rand()/(double)RAND_MAX;
+                A[i][j]=complex(p,q);
             }
         }
-        
-        molecular(A,B,hi,hf);
-        r = (double)rand()/(double)RAND_MAX;
-        if(exp(hi-hf)>r)
-        {
-            c+=1;
-        }
 
-        else
+       // #pragma omp parallel for
+        //metropolis test
+        for(int i=1; i<10000; i+=1)
         {
             for(int j=0; j<n; j+=1)
             {
                 for(int k=0; k<n; k+=1)
-                {
-                    A[j][k] = A0[j][k];
+                {   
+                    //x0=x
+                    A0[j][k] = A[j][k];
+
                 }
             }
+            
+            molecular(A,B,hi,hf);
+            r = (double)rand()/(double)RAND_MAX;
+            a = hf-hi;
+            if(exp(hi-hf)>r)
+            {
+                c+=1;
+            }
+
+            else
+            {
+                for(int j=0; j<n; j+=1)
+                {
+                    for(int k=0; k<n; k+=1)
+                    {
+                        A[j][k] = A0[j][k];
+                    }
+                }
+            }
+
+            s = hamiltonian(A,B);
+            s2 += hamiltonian(A,B);
+            C[i] = s;
+            fout << i << "  " << (double)s/(double)i << "  " << (double)s2/(double)(i) << endl;
+
+            //x = exp(-s/(298*1.38E-23));
+            //file << i << " "  << x << endl;
         }
 
-        s = hamiltonian(A,B);
-        s2 += hamiltonian(A,B);
-        C[i] = s;
-        fout << i << "  " << (double)s/(double)i << "  " << (double)s2/(double)(i) << endl;
+        
+        for(int i=1; i<1000; i+=1)
+        {
+            y1 = exp(-C[i]);
 
-        //x = exp(-s/(298*1.38E-23));
-        //file << i << " "  << x << endl;
-    }
+            file << i << "  " << y1 << endl;
+        }
 
-    for(int i=1; i<10000; i+=1)
-    {
-        y1 = (298*1.38*pow(10,-23));
-        x2 += exp(-C[i]/y1);
-    }
+        for(int i=1; i<10000; i+=1)
+        {
+            y2 = (298);
+            x1 += C[i]*exp(-C[i]/298);
 
-    for(int i=1; i<10000; i+=1)
-    {
-        y2 = (298*1.38*pow(10,-23));
-        x1 += C[i]*exp(-C[i]/y2);
-
-        file << i << "  " << exp(-C[i]/y1) << endl;
-    }
-
+            //file << i << "  " << y1 << endl;
+        }
+   // }
     return 0;
 }
